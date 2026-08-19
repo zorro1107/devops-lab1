7d3b9eda0e2e78834237753dd5b6202a630ab04b
Anirudh Navuduri <122357648+zorro1107@users.noreply.github.com>
2026-08-19 15:39:23 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,7 +1,9 @@
 from app import add, greet
 
+
 def test_add():
-  assert add(2, 3) == 5
+    assert add(2, 3) == 5
+
 
 def test_greet():
-  assert greet("World") == "Hello, World!"
+    assert greet("World") == "Hello, World!"
